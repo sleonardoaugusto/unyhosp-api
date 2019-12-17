0d68cf8ad3200ccb00002236fdca4f9b2cfb2bdc
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-12-17 16:21:35 -0300

    Bed pacient_id is not mandatory

--- unyhosp/core/models.py
+++ unyhosp/core/models.py
@@ -33,7 +33,7 @@ class Bed(models.Model):
     name = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True, blank=True)
     uti = models.ForeignKey(UTI, on_delete=models.CASCADE)
-    pacient = models.ForeignKey(Pacient, on_delete=models.CASCADE)
+    pacient = models.ForeignKey(Pacient, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
