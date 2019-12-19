4783add05a1e59f898a991a5cb50b90f4fd134d3
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-12-19 17:40:40 -0300

    CORS packages added

--- unyhosp/settings.py
+++ unyhosp/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'unyhosp.core',
 ]
 
@@ -47,7 +48,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'unyhosp.urls'
 
