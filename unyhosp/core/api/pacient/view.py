971b20ecff85289afc20f7931f16a0d2517e72c4
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-12-20 15:08:28 -0300

    Pacient resource filter by name, document_id implemented

--- unyhosp/core/api/pacient/view.py
+++ unyhosp/core/api/pacient/view.py
@@ -1,8 +1,19 @@
 from rest_framework import viewsets
+from django_filters import rest_framework
 
 from . import model, serializer
+
+
+class PacientFilter(rest_framework.FilterSet):
+    name = rest_framework.filters.CharFilter(lookup_expr='icontains')
+    document_id = rest_framework.filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = model.Pacient
+        fields = ['name', 'document_id']
 
 
 class PacientViewSet(viewsets.ModelViewSet):
     queryset = model.Pacient.objects.all()
     serializer_class = serializer.PacientSerializer
+    filterset_class = PacientFilter
--- unyhosp/settings.py
+++ unyhosp/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'corsheaders',
     'unyhosp.core',
 ]
@@ -73,6 +74,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'unyhosp.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
