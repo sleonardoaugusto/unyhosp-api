0aae061429a78dd1137e120bc86efc14c0325bb7
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-12-21 18:21:17 -0300

    Date of Birth representation and input format feature

--- unyhosp/core/api/pacient/serializer.py
+++ unyhosp/core/api/pacient/serializer.py
@@ -4,6 +4,8 @@ from . import model
 
 
 class PacientSerializer(serializers.ModelSerializer):
+    date_of_birth = serializers.DateField(format='%d/%m/%Y', input_formats='%d/%m/%Y')
+
     class Meta:
         model = model.Pacient
         fields = '__all__'
