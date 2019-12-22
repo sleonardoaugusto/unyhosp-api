06fdf18c84afe2ae872aa4d7d69e93a767b75023
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-12-22 00:57:51 -0300

    DateField input_formats fix

--- unyhosp/core/api/pacient/serializer.py
+++ unyhosp/core/api/pacient/serializer.py
@@ -4,7 +4,7 @@ from . import model
 
 
 class PacientSerializer(serializers.ModelSerializer):
-    date_of_birth = serializers.DateField(format='%d/%m/%Y', input_formats='%d/%m/%Y')
+    date_of_birth = serializers.DateField(format='%d/%m/%Y', input_formats=['%d/%m/%Y'])
 
     class Meta:
         model = model.Pacient
