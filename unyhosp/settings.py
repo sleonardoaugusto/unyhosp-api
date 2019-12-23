2eb2e78a4abeac9d90539cb10baae1a6ea06beb4
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-12-23 01:58:04 -0300

    MySQL docker connection

--- unyhosp/settings.py
+++ unyhosp/settings.py
@@ -84,8 +84,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unyhosp',
+        'USER': 'admin',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
