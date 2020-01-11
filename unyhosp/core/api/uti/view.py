2444e1e530bf77b64ea23be068edfd93e5e4de24
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-01-11 18:26:15 -0300

    Get UTI by hospital id; Postman collection updated

--- unyhosp/core/api/uti/view.py
+++ unyhosp/core/api/uti/view.py
@@ -1,10 +1,20 @@
 from rest_framework import viewsets
+from django_filters import rest_framework
 
 from . import model, serializer
 
 
+class UTIFilter(rest_framework.FilterSet):
+    hospital = rest_framework.filters.CharFilter(lookup_expr='exact')
+
+    class Meta:
+        model = model.UTI
+        fields = ['hospital_id']
+
+
 class UTIViewSet(viewsets.ModelViewSet):
     queryset = model.UTI.objects.all()
+    filterset_class = UTIFilter
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
